dcaf2ec90ad9ad911c8169c77e1269a9428fcf87
Juanrh71 <142904780+Juanrh71@users.noreply.github.com>
2024-03-08 13:14:18 -0400

    Actualizar app.py

--- Actividad 3/app.py
+++ Actividad 3/app.py
@@ -1,4 +1,4 @@
-from sympy import symbols,Eq,solve
+from sympy import symbols,Eq,solve #Importamos las librerias Sympy y numpy
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
@@ -8,9 +8,9 @@ def read_root():
     x,y,z=symbols("x y z")
     ecuacion1=Eq((2*x-4*y+6*z),20)
     ecuacion2=Eq((5*x-3*y+4*z),20)
-    ecuacion3=Eq((3*x-7*y+5*z),20)
+    ecuacion3=Eq((3*x-7*y+5*z),20) # Definimos las 3 ecuaciones 
 
-    print("Ecuacion 1:", ecuacion1)
+    print("Ecuacion 1:", ecuacion1) 
     print("Ecuacion 2:", ecuacion2)
     print("Ecuacion 3:", ecuacion3)
 
@@ -22,7 +22,7 @@ def read_root():
     x_mesh, y_mesh = np.meshgrid(np.linspace(-10, 10, 50), np.linspace(-10, 10, 50))
     z_mesh1 = (20 - 2*x_mesh + 4*y_mesh) / 6
     z_mesh2 = (20 - 5*x_mesh + 3*y_mesh) / 4
-    z_mesh3 = (20 - 3*x_mesh + 7*y_mesh) / 5
+    z_mesh3 = (20 - 3*x_mesh + 7*y_mesh) / 5 #Operamos las ecuaciones 
 
     figura=plt.figure()
     ax=figura.add_subplot(111,projection="3d")
